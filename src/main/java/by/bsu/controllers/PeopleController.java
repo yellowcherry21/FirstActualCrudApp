637871d75849cbeba9824fe81b0abb110bda911c
Vasilii <mmf.drobysheVA@bsu.by>
2022-02-28 23:45:53 +0300

    Deleted unused import

--- src/main/java/by/bsu/controllers/PeopleController.java
+++ src/main/java/by/bsu/controllers/PeopleController.java
@@ -2,7 +2,6 @@ package by.bsu.controllers;
 
 import by.bsu.dao.PersonDAO;
 import by.bsu.models.Person;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
